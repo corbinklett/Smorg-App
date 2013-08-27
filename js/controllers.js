149bdf64690ced4044f8cd54ce6335a2ec62becf
Corbin Klett <corbinklett@gmail.com>
2013-08-26 22:47:36 -0400

    asdf

--- js/controllers.js
+++ js/controllers.js
@@ -15,9 +15,12 @@ function LoginCtrl($scope, $location, MemberDatabase, $cookies, $rootScope) {
   }
 
   $scope.memberLogin = function() {
+    alert($scope.member.username);
+    alert($scope.member.password);
     $scope.member = MemberDatabase.get({username:$scope.member.username, password:$scope.member.password},
       function(data){
         if (data.user) {
+          alert('database call made');
           $cookies.user = $scope.member.user;
           $cookies.firstname = $scope.member.firstname;
           $cookies.lastname = $scope.member.lastname;
@@ -26,6 +29,7 @@ function LoginCtrl($scope, $location, MemberDatabase, $cookies, $rootScope) {
           $location.path('/following');
         }
         else {
+          alert('error');
           $scope.member.loginErr = 'Incorrect Username/Password';
         } 
       });
