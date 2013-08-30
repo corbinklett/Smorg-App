49778f9f813ede4c9af3b2d621a8c7658bb6e22d
Corbin Klett <corbinklett@gmail.com>
2013-08-29 20:30:44 -0400

    updated profile images

--- js/app.js
+++ js/app.js
@@ -5,6 +5,23 @@
 //angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   
 var smorg = angular.module('smorg', ['ui.bootstrap', 'services', 'ngCookies', 'directives', 'imageupload', 'ngUpload', 'ui.select2', 'LocalStorageModule']);
+/*
+smorg.config(function($routeProvider, $compileProvider) {
+$routeProvider.    
+    when('/login', {templateUrl: 'partials/login.html', controller: LoginCtrl}).
+    when('/signup', {templateUrl: 'partials/signup.html', controller: SignupCtrl}).
+    when('/following', {templateUrl: 'partials/following.html', controller: FollowingCtrl}).
+    when('/city', {templateUrl: 'partials/city.html', controller: CityCtrl}).
+    when('/profile/:id', {templateUrl: 'partials/profile.html', controller: ProfileCtrl}).
+    when('/home', {templateUrl: 'partials/home.html', controller: HomeCtrl}).    
+    when('/search_results/:search_tags/:tag_text', {templateUrl: 'partials/search_results.html', controller:SearchResCtrl}).
+    when('/upload', {templateUrl: 'partials/upload.html', controller: UploadCtrl}).
+    otherwise({redirectTo: '/login'});
+  
+    $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+  });
+*/
+
 
 smorg.config(['$routeProvider', function($routeProvider) {
 $routeProvider.    
@@ -19,5 +36,13 @@ $routeProvider.
     otherwise({redirectTo: '/login'});
   }]);
 
+/*
+angular.module('App', []).config(function ($routeProvider, $compileProvider) {
 
-
+    $routeProvider.when('/test', {
+        controller: TestCtrl,
+        template: '<h1> {{ test }} </h1>'        
+    });
+    $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+});
+*/
